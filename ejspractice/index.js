a00410b4be77b21955f31bb2cd8b49ceacbebc02
YuanjenHung <a0986859219@gmail.com>
2022-01-16 15:05:46 +0100

    first push

---
+++ ejspractice/index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(express.static('assets'));
+
+app.get('/', (req, res) => {
+    res.render('home');
+})
+
+app.get('/random', (req, res) => {
+    const random = Math.floor(Math.random()*10)+1;
+    res.render('random', {rand: random});
+})
+
+app.get('/cats', (req, res) => {
+    const cats = ['Molly', 'Rocket', 'Bubble'];
+    res.render('cats', {cats});
+})
+
+app.get('/r/:subreddits', (req, res) => {
+    res.render('subreddits', {name: req.params.subreddits});
+})
+
+app.listen('3000', () => {
+    console.log("Local server is listening on port 3000!");
+})
